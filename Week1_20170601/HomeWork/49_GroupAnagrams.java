77ee44e0af91c2c86709ab09360fd8ffc92b9f0a
jasontangfocus <jasontangfocus@gmail.com>
2017-06-10 14:14:40 -0500

    leetcode_49
    First modification of leetcode 49

---
+++ Week1_20170601/HomeWork/49_GroupAnagrams.java
@@ -0,0 +1,38 @@
+/*
+Given an array of strings, group anagrams together.
+
+For example, given: ["eat", "tea", "tan", "ate", "nat", "bat"], 
+Return:
+
+[
+  ["ate", "eat","tea"],
+  ["nat","tan"],
+  ["bat"]
+]
+Note: All inputs will be in lower-case.
+*/
+public class 49_GroupAnagrams {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        
+        //Exception case
+        if(strs == null || strs.length == 0) {
+            return new ArrayList<List<String>>();
+        }
+        
+        //Normal case
+        Map<String, List<String>> map = new HashMap<String, List<String>>();
+        
+        for(String str : strs) {
+            //transform each string in the string strs array to char array
+            char[] ca = str.toCharArray();
+            //sort each character 
+            Arrays.sort(ca);
+            String keyStr = String.valueOf(ca);
+            if (!map.containsKey(keyStr)) {
+                map.put(keyStr, new ArrayList<String>());
+            }
+            map.get(keyStr).add(str);
+        }
+        return new ArrayList<List<String>>(map.values());
+    }
+}
