2b2592d95b49448572421d10d73fd6cb53c89ecf
jasontangfocus <jasontangfocus@gmail.com>
2017-06-20 23:57:44 -0500

    commmitted 141

---
+++ Week1_20170601/Algorithm_pratice/141_LinkedListCycle.java
@@ -0,0 +1,43 @@
+/*
+Given a linked list, determine if it has a cycle in it.
+
+Follow up:
+Can you solve it without using extra space?
+*/
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+// idea is we use both fast and slow pointer to track the ListNode
+
+public class 141_LinkedListCycle {
+    public boolean hasCycle(ListNode head) {
+        //Corner Case
+        if (head == null || head.next == null) {
+        	return false;
+        }
+
+        //normal case
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while(fast != null && fast.next != null) {
+        	if (slow == fas) {
+        		return true;
+        	}
+        	slow = slow.next;
+        	fast = fast.next.next;
+        }
+
+        return false;
+    }
+}
