ec020b7eefaee3b0deba046fed38d207e4ebc742
jasontangfocus <jasontangfocus@gmail.com>
2017-06-21 00:33:58 -0500

    Committed 242

---
+++ Week1_20170601/Algorithm_pratice/242_ValidAnagram.java
@@ -0,0 +1,72 @@
+/*
+Given two strings s and t, write a function to determine if t is an anagram of s.
+
+For example,
+s = "anagram", t = "nagaram", return true.
+s = "rat", t = "car", return false.
+
+Note:
+You may assume the string contains only lowercase alphabets.
+
+Follow up:
+What if the inputs contain unicode characters? How would you adapt your solution to such case?
+*/
+
+public class 242_ValidAnagram {
+	public boolean isAnagram(String s, String t) {
+		//corner case
+		if (s.equals(t)) {
+			return true;
+		}
+
+		if (s.length() != t.length()) {
+			return false;
+		}
+
+		//normal case
+		//here we use HashMap
+
+		Map<Character, Integer> map = new HashMap<>();
+
+		for (int i = 0; i < s.length(); i++) {
+			map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+			map.put(t.charAt(i), map.getOrDefault(t.charAt(i), 0) - 1);
+		}
+
+		for (int cur : map.values()) {
+			if (cur != 0) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+}
+
+//*****************Version 2******************
+/*public class 242_ValidAnagram {
+	public boolean isAnagram(String s, String t) {
+		//corner case
+		if (s.equals(t)) {
+			return true;
+		}
+
+		if (s.length() != t.length()) {
+			return false;
+		}
+
+		//normal case
+		//here we use ASCII code to transform to int[] array
+		int[] map = new int[128];
+		for (int i = 0; i < length(); i++) {
+			map[s.charAt(i) - 'a']++;
+			map[t.charAt(i) - 'a']--;
+		}
+		for (int cur : map) {
+			if (cur != 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+}*/
