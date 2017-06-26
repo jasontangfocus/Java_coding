9a01377bd59cf03654f0fe8c25591ef37506f9ee
jasontangfocus <jasontangfocus@gmail.com>
2017-06-26 09:16:36 -0500

    java testing

--- Week1_20170601/Algorithm_pratice/Test/src/com/JohnT/Main.java
+++ Week1_20170601/Algorithm_pratice/Test/src/com/JohnT/Main.java
@@ -1,21 +1,36 @@
 package com.JohnT;
 
+import sun.jvm.hotspot.utilities.Interval;
+
+import java.sql.Time;
+import java.time.Duration;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
 public class Main {
 
     public static void main(String[] args) {
-        float a = (float)0.1;
-        float b = (float)0.2;
-        float c = (float)0.3;
-        System.out.println(a + b - c);
 
+        long starTime = System.nanoTime();
+        for (int i = 0; i < 1000; i++) {
+            for (int j = 0; j < 1000; j++) {
+                for(int k = 0; k < 1000; k++) {
+                    if (i + j + k == 1000 && i * i + j * j == k * k) {
+                        System.out.println("a:" + i + " b:" + j + " c:" + k);
+                    }
+                }
+            }
+        }
 
-        long d = (long)0.1;
-        long e = (long)0.2;
-        long f = (long)0.3;
-        System.out.println(d + e - f);
+        long endTime = System.nanoTime();
+        long totalTime = endTime - starTime;
 
+        Duration duration = Duration.ofNanos(totalTime);
 
+        System.out.println(duration);
 
-        System.out.println(0.1+0.2-0.3);
     }
+
+
 }
