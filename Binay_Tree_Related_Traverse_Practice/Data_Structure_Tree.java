5834338121099bf290694bc86c7ff3e212f77758
jasontangfocus <jasontangfocus@gmail.com>
2017-07-01 16:40:28 -0500

    notes added

---
+++ Binay_Tree_Related_Traverse_Practice/Data_Structure_Tree.java
@@ -0,0 +1,13 @@
+/*
+Tree Concepts:
+
+Root:	The nodes at the top of the trees.
+Parent:	When any node (except the root) has exactly one 		edge runnning upward to another node. The node 			above is called parent of the node.
+Child:	Any node may have one or more lines running  
+		downward to other nodes. These nodes below the given node called its children.
+Edge:	Connection between one node to another.
+Leaf:	A node that has no children is called a leaf. 			There can be only one root in a tree but there 			can be many leaves.
+Levels:	The level of a particular node refers to how 			geenrations the node is from the root. The root 		is level 0 and its children are level 1 and so 			on.
+Height:	The height of a tree is the number of edges on 			the longest downward path between the root and 			a leaf.
+Path:	a sequence of nodes and edges connecting a node 		with a descendant.
+*/
