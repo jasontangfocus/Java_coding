b2a0a3ef72f1e1d742ec74f90150ed5575231dfe
jasontangfocus <jasontangfocus@gmail.com>
2017-07-03 03:03:23 -0500

    fixed

---
+++ Data_Structure/List/ArrayList.java
@@ -0,0 +1,12 @@
+/*
+ArrayList常见方法：
+
+1.	add(object):	adds a new element to the end
+2.	add(index, object):	inserts a new element at the specified index
+3.	set(index, object):	replaces an existing element at the specified index with the new element
+4.	get(index):	returns the element at the specified index
+5.	size(): returns the number of elements
+6.	iterator
+
+
+*/
--- Data_Structure/List/List.java
+++ Data_Structure/List/List.java
@@ -23,17 +23,6 @@ Collection是一个高级接口，它包含了List，Set和Queue三个子接口
 
 */
 
-/*
-ArrayList常见方法：
 
-1.	add(object):	adds a new element to the end
-2.	add(index, object):	inserts a new element at the specified index
-3.	set(index, object):	replaces an existing element at the specified index with the new element
-4.	get(index):	returns the element at the specified index
-5.	size(): returns the number of elements
-6.	iterator
-
-
-*/
 
 
