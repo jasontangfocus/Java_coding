ef963d8dc6a31169f2343d136657da58d525ebb8
jasontangfocus <jasontangfocus@gmail.com>
2017-07-03 03:27:44 -0500

    capacity function of java

--- Data_Structure/List/ArrayList.java
+++ Data_Structure/List/ArrayList.java
@@ -8,5 +8,27 @@ ArrayList常见方法：
 5.	size(): returns the number of elements
 6.	iterator
 
+每个ArrayList实例都有一个容量，当需要插入大量元素时，在插入前可以调用ensureCapacity方法来增加容量以提高插入效率
 
 */
+
+public boolean add(E e) {
+	ensureCapacity(size + 1);
+	elementData[size++] = e;
+	return true;
+}
+
+public void ensureCapacity(int minCapacity) {
+	modCount++;	//维护线程安全用
+	int oldCapacity = elementData.length;
+	if (minCapacity > oldCapacity) {
+		Object oldData[] = elementData;
+		int newCapacity = (oldCapacity * 3) / 2 + 1;
+		if (newCapacity < minCapacity) {
+			newCapacity = minCapacity;
+			elementData = Arrays.copyOf(elementData, newCapacity);
+		}
+	}
+}
+
+//目前java所用的增长函数是 (oldCapacity * 3) / 2 + 1;
