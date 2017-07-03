e0fab1dc9c8e17e88a8e61512c0c36e7ee334315
jasontangfocus <jasontangfocus@gmail.com>
2017-07-03 03:02:22 -0500

    List committed

--- Data_Structure/List/List.java
+++ Data_Structure/List/List.java
@@ -1,8 +1,39 @@
 /*
 List是java的一个接口，有四种常见的实现方法，ArrayList, LinkedList, Vector,和Stack类。
 其中前三种是支持数据的线性存储和定位
+
+Collection是一个高级接口，它包含了List，Set和Queue三个子接口
 */
 
+/*
+实现 集合 接口的类，需要具备以下几个功能：
+	插入
+	删除
+	搜索
+	排序
+	遍历和遍历器
+*/
 
+/*
+一个Collection的遍历器，需要实现Iterable接口，亦即返回一个遍历器对象iterator。
+这个对象支持以下方法：
+	- boolean hasnext()
+	- E	next()
+	- void remove()
+
+*/
+
+/*
+ArrayList常见方法：
+
+1.	add(object):	adds a new element to the end
+2.	add(index, object):	inserts a new element at the specified index
+3.	set(index, object):	replaces an existing element at the specified index with the new element
+4.	get(index):	returns the element at the specified index
+5.	size(): returns the number of elements
+6.	iterator
+
+
+*/
 
 
