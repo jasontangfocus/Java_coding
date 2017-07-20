e7704ca5350ddbd1260dc781784f0be8c5f126d4
jasontangfocus <jasontangfocus@gmail.com>
2017-07-19 23:13:32 -0500

    464

---
+++ Week6_20170706_Lecture/CanIWin464.java
@@ -0,0 +1,100 @@
+/*
+In the "100 game," two players take turns adding, to a running total, any integer from 1..10. The player who first causes the running total to reach or exceed 100 wins.
+
+What if we change the game so that players cannot re-use integers?
+
+For example, two players might take turns drawing from a common pool of numbers of 1..15 without replacement until they reach a total >= 100.
+
+Given an integer maxChoosableInteger and another integer desiredTotal, determine if the first player to move can force a win, assuming both players play optimally.
+
+You can always assume that maxChoosableInteger will not be larger than 20 and desiredTotal will not be larger than 300.
+
+Example
+
+Input:
+maxChoosableInteger = 10
+desiredTotal = 11
+
+Output:
+false
+
+Explanation:
+No matter which integer the first player choose, the first player will lose.
+The first player can choose an integer from 1 up to 10.
+If the first player choose 1, the second player can only choose integers from 2 up to 10.
+The second player will win by choosing 10 and get a total = 11, which is >= desiredTotal.
+Same with other integers chosen by the first player, the second player will always win.
+*/
+
+public class CanIWin464 {
+	public boolean canIWin(int maxChoosableInteger, int desiredTotal) {
+		//Edge Case
+		if (desiredTotal <=0) {
+			return true;
+		}
+
+		if (maxChoosableInteger * (maxChoosableInteger + 1) / 2 < desiredTotal) {
+			return false;
+		}
+
+		return canIWin(desiredTotal, maxChoosableInteger, 0, new HashMap<>());
+	}
+
+	public boolean canIWin(int total, int n, int state, HashMap<Integer, Boolean> cache) {
+		if (total <= 0) {
+			return false;
+		}
+
+		if (cache.containsKey(state)) {
+			return cache.get(state);
+		}
+
+		for (int i = 0; i < n; i++) {
+			if ((state & (1 << i)) != 0) {
+				continue;
+			}
+			if (!canIWin(total - (i + 1), n, state | (1 << i), cache)) {
+				cache.put(state, true);
+				return true;
+			}
+		}
+		cache.put(state, false);
+		return false;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
